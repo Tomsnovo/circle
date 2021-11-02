630c44082019465273ce43dff302c3c374b2c11b
Tomáš Bejček <Bejcek.Tomas@stud.vassboskovice.cz>
2021-11-02 09:27:05 +0100

    Circle

--- circle/Program.cs
+++ circle/Program.cs
@@ -11,43 +11,47 @@ namespace circle
             double circuit;
             double content;
             double input;
+            while (true)
+            { 
+                Console.Clear();
+                Console.SetCursorPosition(5,1);
+                Console.WriteLine("Circle (cm)");
+                Console.WriteLine("---------------------");
+                Console.WriteLine("Enter 1 for radius");
+                Console.WriteLine("Enter 2 for diameter");
+                enter = int.Parse(Console.ReadLine());
 
-            Console.Clear();
-            Console.SetCursorPosition(5,1);
-            Console.WriteLine("Circle (cm)");
-            Console.WriteLine("---------------------");
-            Console.WriteLine("Enter 1 for radius");
-            Console.WriteLine("Enter 2 for diameter");
-            enter = int.Parse(Console.ReadLine());
-
-            if (enter == 1)
-            {
+                if (enter == 1)
+                {
                 Console.Write("Enter radius (r) ");
 
-            }
-            else if (enter == 2)
-            {
+                }
+                else if (enter == 2)
+                {
                 Console.Write("Enter diameter (d) ");
-            }
-            else if (enter <= 0 || enter >= 3)
-            {
+                }
+                else if (enter <= 0 || enter >= 3)
+                {
                 Console.WriteLine("Enter valid numbers");
-            }
-            input = double.Parse(Console.ReadLine());
-            input = Math.Round(input, 1);
-            if (enter == 2)
-            {
+                }
+                input = double.Parse(Console.ReadLine());
+                input = Math.Round(input, 1);
+                if (enter == 2)
+                {
                 input = input / 2;
+                }
+
+                circuit = 2 * pi * input;
+                circuit = Math.Round(circuit, 1);
+
+                content = pi * input * input;
+                content = Math.Round(content, 1);
+
+                Console.WriteLine($"Circuit is {circuit} cm ");
+                Console.WriteLine($"Content is {content} cm ");
+                Console.ReadLine();
+                
             }
-
-            circuit = 2 * pi * input;
-            circuit = Math.Round(circuit, 1);
-
-            content = pi * input * input;
-            content = Math.Round(content, 1);
-
-            Console.WriteLine($"Circuit is {circuit} cm ");
-            Console.WriteLine($"Content is {content} cm ");
 
         }
     }
